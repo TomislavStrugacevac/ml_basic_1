240e358ad8b8b5fb46be9ce220a994bb75fca669
Tomislav Strugačevac <tomislav.strugacevac@gauss.hr>
2020-12-05 22:25:32 +0100

    chore: inital commit

--- main.py
+++ main.py
@@ -2,24 +2,3 @@ import os
 # remove info and warning messages to remove GPU spam
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import tensorflow as tf
-
-# Load and prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-# Build the tf.keras.Sequential model by stacking layers. Choose an optimizer and loss function for training:
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
-# For each example the model returns a vector of "logits" or "log-odds" scores, one for each class.
-predictions = model(x_train[:1]).numpy()
-print(predictions)
-# The tf.nn.softmax function converts these logits to "probabilities" for each class:
-tf.nn.softmax(predictions).numpy()
-
-
